f2cb24df71da84ed1f5900b0c9b7fc0f4934c6a5
ZZZnah <ZZZnah@8e2293a8-97e7-11dd-be93-c58765760c5f>
2009-01-05 15:06:09 +0000

    work on SSN

--- demo/SVORenderer.cpp
+++ demo/SVORenderer.cpp
@@ -9,7 +9,7 @@ SVORenderer::SVORenderer()
 , m_viewSize(640, 480)
 , m_fov(70.0f)
 , m_detailCoef(1.0)
-, m_ditherCoef(1.0f/2048.0f)
+, m_ditherCoef(0/*1.0f/2048.0f*/)
 {
   cudaGetTextureReference(&m_dataTexRef, "nodes_tex");
 
@@ -94,12 +94,14 @@ void SVORenderer::Render(void * d_dstBuf)
   rp.ditherCoef = m_ditherCoef;
   rp.rndSeed = 0;//cg::rand((int)m_noiseBuf.size());
 
+  rp.rays = m_rayDataBuf.d_ptr();
+
   CuSetSymbol(rp, "rp");
 
-  Run_InitEyeRays(make_grid2d(m_viewSize, point_2i(16, 28)), m_rayDataBuf.d_ptr(), m_noiseBuf.d_ptr());
+  Run_InitEyeRays(make_grid2d(m_viewSize, point_2i(16, 28)), m_noiseBuf.d_ptr());
   CUT_CHECK_ERROR("ttt");
-  Run_Trace(make_grid2d(m_viewSize, point_2i(16, 28)), m_rayDataBuf.d_ptr());
+  Run_Trace(make_grid2d(m_viewSize, point_2i(16, 28)));
   CUT_CHECK_ERROR("ttt");
-  Run_ShadeSimple(make_grid2d(m_viewSize, point_2i(16, 16)), m_rayDataBuf.d_ptr(), (uchar4*)d_dstBuf);
+  Run_ShadeSimple(make_grid2d(m_viewSize, point_2i(16, 16)), (uchar4*)d_dstBuf);
   CUT_CHECK_ERROR("ttt");
 }
