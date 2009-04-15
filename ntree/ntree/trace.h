52e602be87a43ca14ea937c3185445ce113cb4db
zzznah <zzznah@8e2293a8-97e7-11dd-be93-c58765760c5f>
2009-04-15 03:30:33 +0000

    broken

--- ntree/ntree/trace.h
+++ ntree/ntree/trace.h
@@ -6,8 +6,16 @@ namespace ntree
 
 struct RayTracer
 {
+  const float dirEps;
+
   int isoLevel;
   const Node * root;
+
+  RayTracer(int isoLevel_, const Node * root_)
+  : dirEps(1e-5)
+  , isoLevel(isoLevel_)
+  , root(root_)
+  {}
 
   // result
   bool hit;
@@ -15,15 +23,36 @@ struct RayTracer
   cg::point_3f normal;
   cg::point_3f color;
 
-  void Trace(point_3f p, point_3f dir)
+  // local
+  point_3i dirSign;
+  point_2f dxy, dyz, dzx; 
+
+  void trace(point_3f p, point_3f dir)
   {
     hit = false;
+    adjustDir(dir);
+    dxy = point_2f
+
+
+
+
     
     
 
   }
 
 
+  void adjustDir(point_3f & dir)
+  {
+    for (int i = 0; i < 3; +i)
+    {
+      float x = dir[i];
+      dirSign[i] = cg::sign(x);
+      if (abs(x) < dirEps)
+        x = cg::sign(x) * dirEps;
+      dir[i] = x;
+    }
+  }
 };
 
 
