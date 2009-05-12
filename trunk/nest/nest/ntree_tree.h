2ddc32244b167bb8aa7d3d6392a76614dd50df07
zzznah <zzznah@8e2293a8-97e7-11dd-be93-c58765760c5f>
2009-05-12 08:14:57 +0000

    tree views seem to work

--- trunk/nest/nest/ntree_tree.h
+++ trunk/nest/nest/ntree_tree.h
@@ -3,6 +3,13 @@
 namespace ntree
 {
 
+struct TreeStat
+{
+  int gridCount;
+  int brickCount;
+  int constCount;
+};
+
 template <class Traits>
 class NTree
 {
@@ -12,26 +19,19 @@ public:
   NTree() { AdjustDepth(1024); }
   ~NTree() {}
 
-  void AdjustDepth(int maxSceneSize)
-  {
-    m_depth = 0;
-    m_extent = Traits::BrickSize;
-    while (m_extent < maxSceneSize)
-    {
-      m_extent *= Traits::GridSize;
-      ++m_depth;
-    }
-  }
-
+  void AdjustDepth(int maxSceneSize);
+ 
   int GetDepth() const { return m_depth; }
   int GetExtent() const { return m_extent; }
 
-  std::string GetStats() const;
+  TreeStat GatherStat() const;
 private:
   struct Node;
   int m_depth;
   int m_extent;
   Node m_root;
+
+  void AccumStat(const Node & node, TreeStat & res) const;
 };
 
 template <class Traits>
@@ -44,15 +44,26 @@ struct NTree<Traits>::Node
   static const int BrickSize3 = BrickSize*BrickSize*BrickSize;
 
   ValueType constVal;
-  Node * nodes;
+  Node * grid;
   ValueType * brick;
 
-  Node() : constVal(Traits::DefValue()), nodes(NULL), brick(NULL) {}
+  enum Type { Grid, Brick, Const };
+
+  Node() : constVal(Traits::DefValue()), grid(NULL), brick(NULL) {}
   ~Node() { MakeConst(Traits::DefValue()); }
+
+  Type GetType() const 
+  { 
+    if (grid != NULL)
+      return Grid;
+    if (brick != NULL)
+      return Brick;
+    return Const;
+  }
 
   void MakeConst(ValueType v)
   {
-    if (nodes != NULL) { delete [] nodes; nodes = NULL; }
+    if (grid != NULL) { delete [] grid; grid = NULL; }
     if (brick != NULL) { delete [] brick; brick = NULL; }
     constVal = v;
   }
@@ -60,21 +71,57 @@ struct NTree<Traits>::Node
   void MakeGrid()
   {
     Assert(brick == NULL);
-    if (nodes != NULL)
+    if (grid != NULL)
       return;
-    nodes = new Node[GridSize3];
-    for (Node * p = nodes; p != nodes + GridSize3; ++p)
+    grid = new Node[GridSize3];
+    for (Node * p = grid; p != grid + GridSize3; ++p)
       p->constVal = constVal;
   }
 
   void MakeBrick()
   {
-    Assert(nodes == NULL);
+    Assert(grid == NULL);
     if (brick != NULL)
       return;
     brick = new ValueType[BrickSize3];
     std::fill(brick, brick + BrickSize3, constVal);
   }
 };
+
+template <class Traits>
+void NTree<Traits>::AdjustDepth(int maxSceneSize)
+{
+  m_depth = 0;
+  m_extent = Traits::BrickSize;
+  while (m_extent < maxSceneSize)
+  {
+    m_extent *= Traits::GridSize;
+    ++m_depth;
+  }
+}
+
+template <class Traits>
+TreeStat NTree<Traits>::GatherStat() const
+{
+  TreeStat stat = {0, 0, 0};
+  AccumStat(m_root, stat);
+  return stat;
+}
+
+template <class Traits>
+void NTree<Traits>::AccumStat(const Node & node, TreeStat & res) const
+{
+  Node::Type type = node.GetType();
+  if (type == Node::Brick)
+    ++res.brickCount;
+  else if (type == Node::Grid)
+    ++res.gridCount;
+  else
+    ++res.constCount;
+
+  if (type == Node::Grid)
+    for (int i = 0; i < Node::GridSize3; ++i)
+      AccumStat(node.grid[i], res);
+}
 
 }
--- trunk/nest/nest/ntree_view.h
+++ trunk/nest/nest/ntree_view.h
@@ -31,7 +31,12 @@ private:
   range_3i m_brickRange;
   range_3i m_range;
 
-  void FetchNode(const Node & node, point_3i pos, int sz);
+  template <bool ToTree>
+  void CopyNode(Node & node, point_3i pos, int sz);
+  template <bool ToBrick>
+  void CopyBrick(ValueType * brick, const point_3i & dstPos);
+  void ShrinkGrid(Node & node);
+  void ShrinkBrick(Node & node);
 };
 
 template<class Traits>
@@ -54,46 +59,100 @@ void NTree<Traits>::View::Unattach()
 }
 
 template<class Traits>
-void NTree<Traits>::View::FetchNode(const Node & node, point_3i pos, int sz)
+template<bool ToTree>
+void NTree<Traits>::View::CopyNode(Node & node, point_3i pos, int sz)
 {
+  Assert(sz >= BrickSize);
   range_3i nodeRange(pos, sz);
   if (!intersect(m_range, nodeRange))
     return;
 
-  if (node.nodes != NULL)
+  Node::Type type = node.GetType();
+  if (type == Node::Grid)
   {
     int sz2 = sz / GridSize;
     for (walk_3 i(GridSize); !i.done(); i.next())
-      FetchNode(node.nodes[i.flat()], pos + i.pos()*sz2, sz2);
+      CopyNode<ToTree>(node.grid[i.flat()], pos + i.pos()*sz2, sz2);
+    if (ToTree)
+      ShrinkGrid(node);
   }
-  else if (node.brick == NULL)
+  else if (type == Node::Const)
   {
-    range_3i dstRange(pos - m_range.p1, sz);
-    range_3i arrayRange(point_3i(), m_data.extent());
-    dstRange &= arrayRange;
-    fill(m_data, dstRange, node.constVal);
+    if (ToTree)
+    {
+      if (sz > BrickSize)
+        node.MakeGrid();
+      else
+        node.MakeBrick();
+      CopyNode<true>(node, pos, sz);
+    } else {
+      range_3i dstRange(pos - m_range.p1, sz);
+      range_3i arrayRange(point_3i(), m_data.extent());
+      dstRange &= arrayRange;
+      fill(m_data, dstRange, node.constVal);
+    }
   }
-  else
+  else // brick
   {
-    //Assert(sz == BrickSize);
-
+    Assert(sz == BrickSize);
+    CopyBrick<ToTree>(node.brick, pos - m_range.p1);
+    if (ToTree)
+      ShrinkBrick(node);
   }
 }
+
+template <class Traits>
+template <bool ToBrick>
+void NTree<Traits>::View::CopyBrick(ValueType * brick, const point_3i & dstPos)
+{
+  for (int z = 0; z < BrickSize; ++z)
+  for (int y = 0; y < BrickSize; ++y)
+  {
+    ValueType * view = m_data.data() + m_data.ofs(dstPos + point_3i(0, y, z));
+    for (int x = 0; x < BrickSize; ++x, ++brick, ++view)
+      ToBrick ? *brick = *view : *view = *brick;
+  }
+}
+
+template <class Traits>
+void NTree<Traits>::View::ShrinkGrid(Node & node)
+{
+  Assert(node.GetType() == Node::Grid);
+  bool allEq = (node.grid[0].GetType() == Node::Const);
+  for (int i = 1; i < GridSize3 && allEq; ++i)
+  {
+    allEq = node.grid[i].GetType() == Node::Const;
+    if (allEq)
+      allEq = (node.grid[i].constVal == node.grid[0].constVal);
+  }
+  if (allEq)
+    node.MakeConst(node.grid[0].constVal);
+}
+
+template <class Traits>
+void NTree<Traits>::View::ShrinkBrick(Node & node)
+{
+  Assert(node.GetType() == Node::Brick);
+  bool allEq = true;
+  for (int i = 1; i < BrickSize3 && allEq; ++i)
+    allEq = (node.brick[i] == node.brick[0]);
+  if (allEq)
+    node.MakeConst(node.brick[0]);
+}
+
 
 template<class Traits>
 void NTree<Traits>::View::Update()
 {
   Assert(m_tree != NULL);
-  FetchNode(m_tree->m_root, point_3i(), m_tree->m_extent);
-
-
-
+  CopyNode<false>(m_tree->m_root, point_3i(), m_tree->m_extent);
 }
 
 template<class Traits>
 void NTree<Traits>::View::Commit()
 {
-
+  Assert(m_tree != NULL);
+  CopyNode<true>(m_tree->m_root, point_3i(), m_tree->m_extent);
 }
 
 }
