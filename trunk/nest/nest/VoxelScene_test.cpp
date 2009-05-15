191c8f5d079c3933504729f5af9d5ced0e422d76
zzznah <zzznah@8e2293a8-97e7-11dd-be93-c58765760c5f>
2009-05-15 00:26:54 +0000

    made test for scene surface generation (build BROKEN)

--- trunk/nest/nest/VoxelScene_test.cpp
+++ trunk/nest/nest/VoxelScene_test.cpp
@@ -1,10 +1,30 @@
 #include "stdafx.h"
 #include "VoxelScene.h"
 
-/*BOOST_AUTO_TEST_CASE( VoxelScene_test )
+void save(array_3d_ref<uint8> & a, const char * fn)
 {
-  VoxelScene scene;
+  std::ofstream file(fn, std::ios::binary);
+  write(file, a.extent());
+  file.write((char *)a.data(), a.size() * sizeof(uint8));
+}
 
+BOOST_AUTO_TEST_CASE( VoxelScene_test )
+{
+  VoxelScene scene(512, 1.0f);
+  offset_geom::Sphere sph(50.0f);
+  offset_geom::Translate tsph(sph);
+  tsph.ofs = point_3f(100, 100, 100);
+  scene.AddSurface(tsph);
+  tsph.ofs = point_3f(160, 100, 100);
+  scene.SubSurface(tsph);
 
+  BOOST_TEST_MESSAGE("SceneStat: " << scene.GatherStat());
 
-}*/
+  array_3d<uint8> buf;
+  range_3f fetchRng(point_3i(0, 0, 100), point_3i(200, 200, 100));
+  scene.FetchAlphaRange(fetchRng, buf);
+  BOOST_CHECK_EQUAL(buf[point_3i(100, 100, 100)], 255);
+  BOOST_CHECK_EQUAL(buf[point_3i(120, 100, 100)], 0);
+
+  save(buf, "VoxelScene_test_slice.dat");
+}
--- trunk/nest/nest/main.cpp
+++ trunk/nest/nest/main.cpp
@@ -1,29 +1,3 @@
 #include "stdafx.h"
 #include "ntree.h"
 
-
-/*void MakeShpere(int size, array_3d<uint8> & dst)
-{
-  point_3i sz(size, size, size);
-  dst.resize(sz);
-  point_3f c = 0.5f * (sz - point_3i(1, 1, 1));
-  const float dl = 2.0f * sqrt(3.0f);
-  const float invDl = 1.0f / dl;
-  const float l0 = 0.5f * size - dl;
-  for (walk_3 i(sz); !i.done(); ++i)
-  {
-    point_3f d = i.p - c;
-    float l = cg::norm(d);
-    float v = 1.0f - (l - l0) * invDl;
-    v = cg::bound(v, 0.0f, 1.0f);
-    dst[i.p] = (uint8)(v*255.0f);
-  }
-}
-
-void save(array_3d_ref<uint8> & a, const char * fn)
-{
-  std::ofstream file(fn, std::ios::binary);
-  write(file, a.extent());
-  file.write((char *)a.data(), a.size() * sizeof(uint8));
-}*/
-
