2e377784fd73d84667f66fc3ec70ab2a0b2c1e78
zzznah <zzznah@8e2293a8-97e7-11dd-be93-c58765760c5f>
2009-06-04 12:14:33 +0000

    ok

--- bittree/main.cpp
+++ bittree/main.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "bittree.h"
+#include <conio.h>
 
 /*#define BOOST_TEST_MODULE BitTreeTest
 #include <boost/test/unit_test.hpp>
@@ -8,10 +9,30 @@ BOOST_AUTO_TEST_CASE( test_test )
 {
 }*/
 
+template <class T>
+void LoadBuf(const char * fn, std::vector<T> & dst)
+{
+  std::ifstream file(fn, std::ios::binary);
+  file.seekg(0, std::ios::end);
+  int sz = file.tellg();
+  file.seekg(0, std::ios::beg);
+  Assert(sz % sizeof(T) == 0);
+  dst.resize(sz / sizeof(T));
+  file.read((char*)&dst[0], sz);
+}
+
+
 int main()
 {
-  RayTracer tracer(NULL, NULL, 0);
-  tracer.trace(point_3f(0.0, 0.5, 0.5), point_3f(1.0, 0.0, 0.0));
-  
+  std::vector<Grid> grids;
+  std::vector<Brick> bricks;
+  LoadBuf("grids.dat", grids);
+  LoadBuf("bit_bricks.dat", bricks);
+
+  RayTracer tracer(&bricks[0], &grids[0], (int)grids.size() - 1);
+  point_3f target(0.5f, 0.5f, 0.5f);
+  point_3f eye(3, 2, 1);
+  tracer.trace(eye, target - eye);
+  getch();
   return 0;
 }
--- bittree/trace.py
+++ bittree/trace.py
@@ -11,7 +11,7 @@ def load_htree():
 
 def make_rays(sx, sy, viewdir, fov):
     def norm_vec(v):
-        return v / norm(v)
+        return v / linalg.norm(v)
     fwd = norm_vec(viewdir)
     right = norm_vec( cross(viewdir, array([0.0, 0.0, 1.0])) )
     up = cross(right, fwd)
@@ -28,7 +28,7 @@ def make_rays(sx, sy, viewdir, fov):
 def trace(htree, eye, target):
     eye = array(eye, float32)
     target = array(target, float32)
-    rays = make_rays(800, 600, target - eye, 70)
+    rays = make_rays(512, 384, target - eye, 70)
     (bricks, grids, root) = htree
     hit = zeros((rays.shape[0], rays.shape[1]), int32)
     voxel_pos = zeros((rays.shape[0], rays.shape[1], 3), int32)
@@ -44,15 +44,11 @@ def trace(htree, eye, target):
         point_3f dir(rays[3*i], rays[3*i+1], rays[3*i+2]);
         tracer.trace(veye, dir);
         hit[i] = tracer.hit ? 1 : 0;
-        if (i == (rayNum + Nrays[1]) / 2)
-        {
-          printf("%d %f, %f, %f\\n", i, dir.x, dir.y, dir.z);
-          printf("%f, %f, %f -- %d\\n", veye.x, veye.y, veye.z, hit[i]);
-        }
         reinterpret_cast<point_3i*>(voxel_pos)[i] = tracer.voxelPos;
       }
     '''
-    weave.inline(code, ["bricks", "grids", "root", "eye", "rays", "voxel_pos", "hit"], headers=['"stdafx.h"', '"bittree.h"'], include_dirs=['.', '../cpp'], force=1)
+    args = ["bricks", "grids", "root", "eye", "rays", "voxel_pos", "hit"]
+    weave.inline(code, args, headers=['"stdafx.h"', '"bittree.h"'], include_dirs=['.', '../cpp'], force=1, compiler='msvc')
     return hit, voxel_pos
 
 
@@ -62,7 +58,7 @@ if __name__ == '__main__':
     
 #    eye = array([1.3, 1.5, 1.1])
 #    target = array([0.5, 0.5, 0.5])
-    eye = array([5.0, 0.5, 0.5])
+    eye = array([2.0, 3.0, 1.0])
     target = array([0.5, 0.5, 0.5])
     hit, voxel_pos = trace(htree, eye, target)
     imshow(hit)
