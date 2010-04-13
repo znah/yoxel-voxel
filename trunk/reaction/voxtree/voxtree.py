85dbdbaf71bbb41e45c7ca8b9968d3c62fbae427
zzznah <zzznah@8e2293a8-97e7-11dd-be93-c58765760c5f>
2010-04-13 13:09:54 +0000

    broken

--- trunk/reaction/voxtree/voxtree.py
+++ trunk/reaction/voxtree/voxtree.py
@@ -28,6 +28,13 @@ def profile_run(f):
 
 
 class App(ZglAppWX):
+    z = Range(0, 512, 100)
+
+    @on_trait_change('z')
+    def updateSlice(self):
+        pass
+        
+
 
     def __init__(self):
         ZglAppWX.__init__(self, viewControl = OrthoCamera(), zglpath='..')
@@ -40,6 +47,7 @@ class App(ZglAppWX):
         v, f = load_obj("../t.obj")
 
         with voxelizer:
+            clearGLBuffers()
             drawArrays(GL_TRIANGLES, verts = v, indices = f)
         
         gl2cudaBuf = cuda_gl.BufferObject(voxelizer.dumpToPBO().handle)
@@ -49,12 +57,16 @@ class App(ZglAppWX):
         MarkBricks  = mod.get_function('MarkBricks') 
         PackBricks  = mod.get_function('PackBricks') 
         UpdateBrickPool  = mod.get_function('UpdateBrickPool') 
+        FetchTest   = mod.get_function('FetchTest') 
         d_slot2slice = mod.get_global('slot2slice')[0]
+        brick_pool_tex = brick_pool_tex = mod.get_texref('brick_pool_tex')
+        brick_grid_tex = brick_grid_tex = mod.get_texref('brick_grid_tex')
 
         print CalcDensity.num_regs, CalcDensity.shared_size_bytes
         print MarkBricks.num_regs, MarkBricks.shared_size_bytes
         print PackBricks.num_regs, PackBricks.shared_size_bytes
         print UpdateBrickPool.num_regs, UpdateBrickPool.shared_size_bytes
+        print FetchTest.num_regs, FetchTest.shared_size_bytes
 
         # sum 2x2x2 bits
         d_density = ga.zeros((size/2, size/2, size/2*4/32), uint32)
@@ -64,9 +76,12 @@ class App(ZglAppWX):
         profile_run(f)
         gl2cudaMap.unmap()
 
+        save("coral", d_density.get())
+        return
+
         # mark and enumerate non-uniform bricks
         grid_size = size / 2 / 4
-        d_bricks      = ga.zeros((grid_size, grid_size, grid_size), uint32)
+        d_bricks = d_bricks = ga.zeros((grid_size, grid_size, grid_size), uint32)
         d_columnStart = ga.zeros((grid_size, grid_size), uint32)
         def f():
             MarkBricks(d_density, d_bricks, d_columnStart, block = (grid_size/2, 1, 1), grid = (grid_size, grid_size))
@@ -96,28 +111,39 @@ class App(ZglAppWX):
               grid = (brick_pool.pool_shape[2] * len(brick_pool.slot2slice), brick_pool.pool_shape[1]))
         profile_run(f)
 
-        print  brick_pool.slot2slice
-
-
-        save("slots", brick_pool.d_map_slots.get())
+        #save("slots", brick_pool.d_map_slots.get())
         brick_pool.commit()
 
-        save('mark', brick_pool.d_map_mark_enum.get())
-        save('bricks', d_bricks.get())
+        # test fetch
+        cu.bind_array_to_texref( brick_pool.d_pool_array, brick_pool_tex )
+        d_bricks.bind_to_texref( brick_grid_tex )
+        self.brick_pool = brick_pool # PROTECT FROM GARBAGE COLLECTION
+        self.d_bricks = d_bricks     # PROTECT FROM GARBAGE COLLECTION
 
-        
-        
+        brick_pool_tex.set_filter_mode( cu.filter_mode.LINEAR )
 
+        d_res = ga.zeros((1024, 1024, 4), uint8)
+        tex = Texture2D(size = (1024, 1024))
+        texFrag = genericFP('tex2D(texture, tc0.xy)')
+        texFrag.texture = tex
+
+        def updateSlice():
+            with cuprofile('updateSlice'):
+                FetchTest(float32(0), float32(0), float32(self.z), d_res, block=(16, 16, 1), grid = (64, 64), 
+                    texrefs = [brick_pool_tex, brick_grid_tex])
+            with tex:
+                glTexSubImage2D(tex.Target, 0, 0, 0, 1024, 1024, GL_RGBA, GL_UNSIGNED_BYTE, d_res.get())
+            self.fp = texFrag
+        self.updateSlice = updateSlice
+        updateSlice()
 
         print total
-        save('colsum', d_columnStart.get() )
-        save('packed', d_packedBricks.get())
-
     
-    #def display(self):
-        #clearGLBuffers()
-        #with ctx(self.viewControl.with_vp, self.fragProg(z = self.time*0.1%1.0)):  #self.z)):
-        #    drawQuad()
+    def display(self):
+        clearGLBuffers()
+
+        with ctx(self.viewControl.with_vp, self.fp):
+            drawQuad()
 
 if __name__ == "__main__":
     App().run()
