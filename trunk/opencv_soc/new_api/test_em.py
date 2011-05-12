596ff983acbc8b6cb7e98e07327e11ef0834f389
zzznah <zzznah@8e2293a8-97e7-11dd-be93-c58765760c5f>
2011-05-12 14:04:24 +0000

    expectation-maximization sample

--- trunk/opencv_soc/new_api/test_em.py
+++ trunk/opencv_soc/new_api/test_em.py
@@ -3,39 +3,57 @@ from numpy import random
 import cv, cv2
 
 
-cluster_n = 5
 
-points = []
-ref_means = []
-ref_covs = []
-for i in xrange(cluster_n):
-    mean = random.rand(2) * 512
-    a = (random.rand(2, 2)-0.5)*100
-    cov = np.dot(a.T, a)
-    n = random.randint(1000)
-    pts = random.multivariate_normal(mean, cov, n)
-    points.append( pts )
-    ref_means.append( mean )
-    ref_covs.append( cov )
-points = np.float32( np.vstack(points) )
+def make_gaussians(cluster_n, img_size):
+    points = []
+    ref_distrs = []
+    for i in xrange(cluster_n):
+        mean = random.rand(2) * img_size
+        a = (random.rand(2, 2)-0.5)*img_size*0.1
+        cov = np.dot(a.T, a) + img_size*0.05*np.eye(2)
+        n = 100+random.randint(900)
+        pts = random.multivariate_normal(mean, cov, n)
+        points.append( pts )
+        ref_distrs.append( (mean, cov) )
+    points = np.float32( np.vstack(points) )
+    return points, ref_distrs
 
-em = cv2.EM(points, params = dict( nclusters = cluster_n, cov_mat_type = cv2.EM_COV_MAT_GENERIC) )
-print em.getMeans(**{})
-
-covs = np.zeros((cluster_n, 2, 2), np.float32)
-covs = em.getCovs(covs)
-
-
-img = np.zeros((512, 512, 3), np.uint8)
-cv.Circle(img, (100, 100), 50, (0, 255, 0))
-cv2.imshow('em_test', img)
-cv2.waitKey(0)
+def draw_gaussain(img, mean, cov, color):
+    x, y = np.int32(mean)
+    w, u, vt = cv2.SVDecomp(cov)
+    ang = np.rad2deg( np.arctan2(u[1, 0], u[0, 0]) )
+    s1, s2 = np.sqrt(w)*3.0
+    cv.Ellipse(img, (x, y), (s1, s2), ang, 0, 360, color)
 
 
+if __name__ == '__main__':
+    cluster_n = 5
+    img_size = 512
 
-#x, y = points.T
-#pl.plot(x, y, '.')
-#pl.show()
+    print 'press any key to update distributions\n'
 
+    while True:
+        print 'sampling distributions...'
+        points, ref_distrs = make_gaussians(cluster_n, img_size)
 
+        print 'EM...'
+        em = cv2.EM(points, params = dict( nclusters = cluster_n, cov_mat_type = cv2.EM_COV_MAT_GENERIC) )
+        means = em.getMeans(**{})
+        covs = np.zeros((cluster_n, 2, 2), np.float32)
+        covs = em.getCovs(covs)
+        found_distrs = zip(means, covs)
+        
+        print 'ready!\n'
 
+        img = np.zeros((img_size, img_size, 3), np.uint8)
+        for x, y in np.int32(points):
+            cv.Circle(img, (x, y), 1, (255, 255, 255), -1)
+        for m, cov in ref_distrs:
+            draw_gaussain(img, m, cov, (0, 255, 0))
+        for m, cov in found_distrs:
+            draw_gaussain(img, m, cov, (0, 0, 255))
+
+        cv2.imshow('em_test', img)
+        ch = cv2.waitKey(0)
+        if ch == 27:
+            break
